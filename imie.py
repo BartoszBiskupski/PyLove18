07744b27f2952a6a6f6dae3189703adfaeb7c3d9
Bartosz Biskupski <bart.biskupski@gmail.com>
2017-12-16 19:04:42 +0100

    nowe zadania

--- imie.py
+++ imie.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello():
-    return render_template("imie.html", name = request.args.get("name", ""))
+    return render_template("imie.html", name = request.args.get("name", ''))
 
 
 app.run(debug=True)
